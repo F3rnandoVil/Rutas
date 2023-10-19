a7579732f4b6e18abf54c98c841bd0a5c18ddeee
F3rnandoVil <ingfernandovv@gmail.com>
2023-10-18 23:01:38 -0600

    Añadi script
    añadi mi archivo base de js

---
+++ script.js
@@ -0,0 +1,37 @@
+class Base {
+    constructor(){
+        this.siguiente = null
+    }
+}
+class Ruta {
+    constructor(){
+        this.primero = null
+    }
+    agregar(){
+        
+    }
+    buscar(){
+
+    }
+    eliminar(){
+
+    }
+    listar(){
+
+    }
+    _listarRec(){
+
+    }
+    listarInv(){
+
+    }
+    _listarInvRec(){
+
+    }
+    agregarInicio(){
+
+    }
+    creaRuta(){
+
+    }
+}
